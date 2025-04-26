46999390e2291b2c78479f02a17aedfdfe003985
amar-gohil <amar7gohil@gmail.com>
2025-04-26 22:20:25 +0530

    client

---
+++ client-gateway/src/main/java/org/client/gateway/ClientGatewayApplication.java
@@ -0,0 +1,13 @@
+package org.client.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientGatewayApplication.class, args);
+    }
+
+}
